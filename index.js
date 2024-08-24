4fa9a1e7e1bdd6c46903be5107a8cd4c62703c42
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2024-08-24 23:20:56 +0530

    Last part

--- index.js
+++ index.js
@@ -4,13 +4,19 @@
 // })
 
 const image = document.querySelector(".fixedImage");
-const imageShow = document.querySelector(".projectContainer");
-imageShow.addEventListener("mouseenter",function(){
-    image.style.display ="block";
-})
-imageShow.addEventListener("mouseleave",function(){
-    image.style.display ="none";
-})
+const projects = document.querySelectorAll(".SingleProject");
+
+projects.forEach(project => {
+    project.addEventListener("mouseenter", function() {
+        const imageUrl = project.getAttribute("myImage");
+        image.style.backgroundImage = `url(${imageUrl})`;
+        image.style.opacity = "1";
+    });
+
+    project.addEventListener("mouseleave", function() {
+        image.style.opacity = "0";
+    });
+});
 
 const project = document.querySelectorAll(".SingleProject");
 console.log(project);
@@ -20,6 +26,32 @@ project.forEach(function(e){
         image.style.backgroundImage= `url(${showimage})`;
     })
 })
+
+
+document.querySelectorAll('.headingName').forEach(header => {
+    header.addEventListener('click', function() {
+        // Remove 'active' class from all headers
+        document.querySelectorAll('.headingName').forEach(h => h.classList.remove('active'));
+        
+        // Add 'active' class to the clicked header
+        this.classList.add('active');
+        
+        // Get the image URL from the clicked header's 'myImage' attribute
+        const imageUrl = this.getAttribute('myPhoto');
+        console.log(imageUrl);
+        
+        //Get the Description from the clicked header's 'myText' attribute
+        const imageDecription = this.getAttribute('myText');
+        document.querySelector(".headingDescription").innerHTML=imageDecription;
+
+        // Set the background image of the #image-display div
+        document.querySelector('.headingImage').style.backgroundImage = `url(${imageUrl})`;
+    });
+});
+
+// Optionally, you can trigger the click event on the first header to show an image by default
+document.querySelector('.header').click();
+
 
 // var loader = document.querySelector(".loader")
 
